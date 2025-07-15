afdd10ca4f7cc27d545562a1eb30a4ec850fd4b0
Alfonsius Tolan Hera <alfontolan4@gmail.com>
2025-07-15 13:06:38 +0700

    page(home) : Refactoring code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
+AOS.init({
+  duration: 1000,
+  once: true,
+});
+
 const menuToggle = document.getElementById("menuToggle");
 const mobileMenu = document.getElementById("mobile-menu");
+const iconHamburger = document.getElementById('iconHamburger');
+const iconClose = document.getElementById('iconClose');
 
 menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
+  iconHamburger.classList.toggle("hidden");
+  iconClose.classList.toggle("hidden");
 });
+

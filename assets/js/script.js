ab2093f67abd403acc710c169e00f95f059126cc
Alfonsius Tolan Hera <alfontolan4@gmail.com>
2025-07-18 12:53:44 +0700

    feat(profile) : Add hero section, and Brochure button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ AOS.init({
 
 const menuToggle = document.getElementById("menuToggle");
 const mobileMenu = document.getElementById("mobile-menu");
-const iconHamburger = document.getElementById('iconHamburger');
-const iconClose = document.getElementById('iconClose');
+const iconHamburger = document.getElementById("iconHamburger");
+const iconClose = document.getElementById("iconClose");
 
 menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
@@ -14,3 +14,21 @@ menuToggle.addEventListener("click", () => {
   iconClose.classList.toggle("hidden");
 });
 
+const openBtn = document.getElementById("openBrosur");
+const closeBtn = document.getElementById("closeBrosur");
+const modal = document.getElementById("brosurModal");
+
+function toggleModal() {
+  const isHidden = modal.classList.contains("hidden");
+  modal.classList.toggle("hidden");
+  modal.classList.toggle("flex", isHidden);
+}
+
+openBtn.addEventListener("click", toggleModal);
+closeBtn.addEventListener("click", toggleModal);
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    toggleModal();
+  }
+});
